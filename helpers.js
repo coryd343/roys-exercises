f924c3ce63a99a0bc62bd3cf6aa43dca996e4138
coryd343 <fizzy.gutsnipe@outlook.com>
2024-08-22 20:29:54 -0700

    Starting code for sorting algorithm exercises.

---
+++ helpers.js
@@ -0,0 +1,46 @@
+export function log(text) {
+    console.log(text);
+}
+
+/**
+ * Creates a cat with the given parameters
+ * @param {string} name 
+ * @param {string} breed 
+ * @param {string} age 
+ * @returns 
+ */
+export function createCat(name, breed, age) {
+    return {name: name, breed: breed, age: age};
+}
+
+/**
+ * Generates a random integer between 1 and the given max number (inclusive).
+ * @param {number} max 
+ * @returns 
+ */
+export function getRandomInt(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+export function getRandomCat() {
+    const names = ['Fluffy', 'Mr. Bojangles', 'SerialKiller', 'Numpty', 'Margaret Thatcher'];
+    const age = getRandomInt(30);
+    const breeds = ['Russian Blue', 'Mexican Green', 'Canadian Dry', 'Burnt Umber', 'Pumpkin Spice'];
+
+    return createCat(names[getRandomInt(names.length - 1)], age, breeds[getRandomInt(breeds.length - 1)]);
+}
+
+export function getRandomCatArray(arrayLength) {
+    let catArray = [];
+    for (let i = 0; i < arrayLength; i++) {
+        catArray.push(getRandomCat());
+    }
+    return catArray;
+}
+
+export function swap(array, indexOne, indexTwo) {
+    const placeHolder = array[indexOne];
+    theArray[indexOne] = theArray[indexTwo];
+    theArray[indexTwo] = placeHolder;
+    return array;
+}
--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-function log(text) {
-    console.log(text);
+import {log, getRandomCatArray} from './helpers.js';
+
+document.getElementById('testButton').addEventListener('click', test);
+
+/**
+ * Test your functions here!
+ */
+export function test() {
+    //HW: Sort this array according by age ascending (ie youngest cat to oldest cat)
+    const allTheCats = getRandomCatArray(5);
+
+    allTheCats = sortCatsByAge(allTheCats);
+    
+    log(JSON.stringify(allTheCats));
 }
 
-function test() {
-    log("hello world");
+///ADD YOUR FUNCTIONS HERE///
+
+function sortCatsByAge(catArray) {
+    return catArray;
 }
